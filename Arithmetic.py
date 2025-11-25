8f787f9cebf41929e578f486ffb6240b4ecca332
bandu007 <bandu007@gmail.com>
2025-11-25 21:22:15 +0530

    I have added the python files

---
+++ Arithmetic.py
@@ -0,0 +1,13 @@
+''' Now I am going to perform the arithmetic operation'''
+num1 = int(input ('Enter the 1st number: '))
+num2 = int(input ('Enter the 2nd number: '))
+
+Add = num1+num2
+sub = num1-num2
+Div = num1/num2
+Mul = num1*num2
+
+print('Addition:', Add)
+print('Subtraction:',sub)
+print('Division:',Div)
+print('Multiplication:',Mul)
