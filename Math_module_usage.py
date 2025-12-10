fe448c07a99ec302f97fa62cc85868744eeb4ce7
bandu007 <bandu007@gmail.com>
2025-12-10 20:31:16 +0530

    Assignment3 file2

---
+++ Math_module_usage.py
@@ -0,0 +1,10 @@
+import math
+
+number = int(input("Enter a number :"))
+squareroot = math.sqrt(number)
+log = math.log(number,math.e)
+sine = math.sin(number)
+
+print(f"Square root : {squareroot}")
+print(f"Logarithm : {log}")
+print(f"Sine: {sine}")
