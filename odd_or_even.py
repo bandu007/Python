27925ab9118ffdc85b1d0eab2853f8c3f413d215
bandu007 <bandu007@gmail.com>
2025-12-12 10:32:35 +0530

    Delete odd_or_even.py

--- odd_or_even.py
+++
@@ -1,5 +0,0 @@
-# This program is to check if the number is odd or even
-
-num = int(input("Enter a number "))
-
-print(f"{num} is an even number") if num % 2 == 0 else print(f"{num} is an odd number")
