c9fb1695cc7f4781b845fa5f9635f4a8b2aaffd9
bandu007 <bandu007@gmail.com>
2025-12-12 10:32:56 +0530

    Delete sumofnumbers.py

--- sumofnumbers.py
+++
@@ -1,9 +0,0 @@
-# This is a program to calculate the sum of numbers from 1 to 50 using for loop
-
-L1 = []
-
-for i in range (1,51):
-    L1.append(i)
-finalsum = sum(L1)
-
-print("The sum of numbers from 1 to 50 is",finalsum)
