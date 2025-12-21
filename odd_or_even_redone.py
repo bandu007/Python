8f9dee26f7ed5f39cb4c46cbeae0aac60ee82a2b
bandu007 <bandu007@gmail.com>
2025-12-21 18:49:58 +0530

    Cleaning the branch

--- odd_or_even_redone.py
+++
@@ -1,8 +0,0 @@
-# This program is to check if the number is odd or even
-
-num = int(input("Enter a number: "))
-
-if num % 2 == 0:
-    print(f"{num} is an even number.")
-else:
-    print(f"{num} is an odd number.")
--- sumofnumbers_redone.py
+++
@@ -1,9 +0,0 @@
-# This is a program to calculate the sum of numbers from 1 to 50 using for loop
-
-finalsum = 0
-
-for i in range (1,51):
-    finalsum = finalsum + i
-
-
-print("The sum of numbers from 1 to 50 is",finalsum)
